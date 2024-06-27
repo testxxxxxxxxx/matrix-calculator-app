bb7d6244abce98531ea845aee3f8bc64d860a79c
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-06-27 16:30:21 +0000

    created fill_matrix method

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,29 +1,50 @@
+use std::io;
+
 pub struct Matrix{
 
     rows: u32,
     col: u32,
-    matrix: Vec<usize>,
+    matrix: Vec<Vec<usize>>
 
 }
 
 trait MatrixMethods {
 
-    fn new(rows: u32, col: u32, matrix: Vec<usize>) -> Self;
+    fn new(rows: u32, col: u32) -> Self;
 
-    fn create(&mut self);
+    fn fill_matrix(&mut self);
 
 }
 
 impl MatrixMethods for Matrix {
 
-    fn new(rows: u32, col: u32, matrix: Vec<usize>) -> Self {
+    fn new(rows: u32, col: u32) -> Self {
 
-        Matrix { rows: rows, col: col, matrix: matrix}
+        Matrix { rows: rows, col: col, matrix: Vec::new() }
 
     }
-    fn create(&mut self){
+    fn fill_matrix(&mut self){
 
-        self.matrix = vec![0; (self.rows * self.col).try_into().unwrap()];
+        let mut input_line = String::new();
+        let mut value: usize = 0;
+
+        for _i in 0..self.rows { 
+
+           let mut vec: Vec<usize> = vec![0; self.rows.try_into().unwrap()];
+
+           for _j in 0..self.col {
+
+                let _ = io::stdin().read_line(&mut input_line);
+
+                value = input_line.trim().parse().expect("Input not an integer");
+
+                vec.push(value);
+
+           }
+
+           self.matrix.push(vec);
+
+        }
 
     }
 
